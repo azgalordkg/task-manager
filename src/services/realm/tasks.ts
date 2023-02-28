28c05dcadf78f84d1940eb8debe2c3bfc3ee7699
azamataltymyshev <aza.alt.com@gmail.com>
2023-02-28 15:50:34 +0600

    add monthly and yearly recurring tasks

--- src/services/realm/tasks.ts
+++ src/services/realm/tasks.ts
@@ -109,14 +109,50 @@ export const prepareUpdateRecurringData = (task: TasksResponseItem) => {
   if (task.startDate && task.endDate) {
     start.set('date', today.date());
     start.set('month', today.month());
+    start.set('year', today.year());
     end.set('date', today.date());
     end.set('month', today.month());
+    end.set('year', today.year());
 
     updateData.startDate = start.toDate();
     updateData.endDate = end.toDate();
   }
 
   return updateData;
+};
+
+const checkMonthlyTaskUpdate = (today: moment.Moment, start: moment.Moment) => {
+  const monthDifference = today.diff(start, 'months');
+
+  if (monthDifference > 0) {
+    const endOfMonth = moment().endOf('month');
+    const isTodayEnd = today.isSame(endOfMonth, 'day');
+
+    const startDate = start.toDate().getDate();
+    const todayDate = today.toDate().getDate();
+
+    if (startDate === todayDate || (isTodayEnd && startDate > todayDate)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const checkYearlyTaskUpdate = (today: moment.Moment, start: moment.Moment) => {
+  const yearDifference = today.diff(start, 'years');
+
+  if (yearDifference > 0) {
+    const isEndOfLeapFebruary =
+      start.toDate().getMonth() === 1 && start.toDate().getDate() >= 28;
+    const isTodayLastOfFebruary =
+      today.toDate().getMonth() === 1 && today.toDate().getDate() >= 28;
+
+    if (isEndOfLeapFebruary && isTodayLastOfFebruary) {
+      return true;
+    }
+  }
+  return false;
 };
 
 export const updateRecurringDates = (
@@ -134,10 +170,19 @@ export const updateRecurringDates = (
       const isToday = moment(new Date(Number(day))).isSame(today, 'day');
 
       if (isToday) {
-        let isBefore = start.isBefore(today);
+        let isBefore = false;
 
+        if (type === 'Daily') {
+          isBefore = start.isBefore(today);
+        }
         if (type === 'Weekly') {
           isBefore = today.diff(start, 'days') % 7 === 0;
+        }
+        if (type === 'Monthly') {
+          isBefore = checkMonthlyTaskUpdate(today, start);
+        }
+        if (type === 'Yearly') {
+          isBefore = checkYearlyTaskUpdate(today, start);
         }
 
         if (isBefore) {
@@ -156,6 +201,12 @@ export const updateRecurringTasks = (tasksByDays: TasksList) => {
   const weeklyTasks = getRecurringTasks(
     'Weekly',
   ) as unknown as TasksResponseItem[];
+  const monthlyTasks = getRecurringTasks(
+    'Monthly',
+  ) as unknown as TasksResponseItem[];
+  const yearlyTasks = getRecurringTasks(
+    'Yearly',
+  ) as unknown as TasksResponseItem[];
 
   const days = Object.keys(tasksByDays);
   const today = moment();
@@ -163,6 +214,8 @@ export const updateRecurringTasks = (tasksByDays: TasksList) => {
 
   updateRecurringDates(dailyTasks, today, days, 'Daily');
   updateRecurringDates(weeklyTasks, today, days, 'Weekly');
+  updateRecurringDates(monthlyTasks, today, days, 'Monthly');
+  updateRecurringDates(yearlyTasks, today, days, 'Yearly');
 };
 
 export const markTaskAsDone = (_id: string, isDone: boolean) => {
