7a68a34c3b73652ab7cd0aac106dd6d870a006ce
Ruslan <boogaaga@gmail.com>
2023-03-21 15:25:24 +0600

    fix bug time error when quickly creating a task

--- src/components/forms/QuickTaskForm/QuickTaskForm.tsx
+++ src/components/forms/QuickTaskForm/QuickTaskForm.tsx
@@ -17,6 +17,29 @@ import styles from './QuickTaskForm.styles';
 import { Props } from './QuickTaskForm.types';
 
 export const QuickTaskForm: FC<Props> = ({ date, handleShowInput }) => {
+  const createDateTime = new Date();
+
+  const combineStartDate = (dateDay: Date, dateTime: Date) => {
+    const day = dateDay.getDate();
+    const month = dateDay.getMonth();
+    const year = dateDay.getFullYear();
+
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes();
+    const seconds = dateTime.getSeconds();
+
+    return new Date(year, month, day, hours, minutes, seconds);
+  };
+
+  const startDate = combineStartDate(new Date(+date), createDateTime);
+  const endDate = new Date();
+
+  startDate.setMinutes(startDate.getMinutes() < 30 ? 0 : 30);
+  endDate.setHours(
+    startDate.getHours() + 1,
+    startDate.getMinutes() < 30 ? 0 : 30,
+  );
+
   const { fetchList } = useTaskModalContext();
   const { theme, activeTheme } = useThemeContext();
   const iconColor = activeTheme === 'light' ? COLORS.GREY : COLORS.GREY_MEDIUM;
@@ -35,8 +58,8 @@ export const QuickTaskForm: FC<Props> = ({ date, handleShowInput }) => {
   const onSubmit = (data: FieldValues) => {
     createTask({
       ...data,
-      startDate: new Date(+date),
-      endDate: new Date(),
+      startDate,
+      endDate,
       repeat: 'Never',
       hasDeadline: false,
       description: '',
