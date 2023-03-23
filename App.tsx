9423fd522787e42f521045f55b49742bc8017acc
azamataltymyshev <aza.alt.com@gmail.com>
2023-03-23 13:35:19 +0300

    remove unused library

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { FC } from 'react';
-import { EventProvider } from 'react-native-outside-press';
 
 import {
   TagManageProvider,
@@ -25,51 +24,42 @@ import { RootStackParamList } from '@/types/navigation';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: FC = () => {
-  const eventProviderStyles = { flex: 1 };
   return (
-    <EventProvider style={eventProviderStyles}>
-      <TaskListProvider>
-        <TagManageProvider>
-          <ThemeProvider>
-            <NavigationContainer>
-              <Stack.Navigator
-                initialRouteName="Home"
+    <TaskListProvider>
+      <TagManageProvider>
+        <ThemeProvider>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Group>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="AboutUs" component={AboutUs} />
+                <Stack.Screen name="Settings" component={Settings} />
+                <Stack.Screen name="Documents" component={DocumentsScreen} />
+                <Stack.Screen
+                  name="TagsSettings"
+                  component={TagsSettingsScreen}
+                />
+                <Stack.Screen name="Theme" component={Theme} />
+              </Stack.Group>
+              <Stack.Group
                 screenOptions={{
-                  headerShown: false,
+                  presentation: 'modal',
+                  contentStyle: { backgroundColor: 'transparent' },
                 }}>
-                <Stack.Group>
-                  <Stack.Screen name="Home" component={HomeScreen} />
-                  <Stack.Screen name="AboutUs" component={AboutUs} />
-                  <Stack.Screen name="Settings" component={Settings} />
-                  <Stack.Screen name="Documents" component={DocumentsScreen} />
-                  <Stack.Screen
-                    name="TagsSettings"
-                    component={TagsSettingsScreen}
-                  />
-                  <Stack.Screen name="Theme" component={Theme} />
-                </Stack.Group>
-                <Stack.Group
-                  screenOptions={{
-                    presentation: 'modal',
-                    contentStyle: { backgroundColor: 'transparent' },
-                  }}>
-                  <Stack.Screen
-                    name="CreateTask"
-                    component={CreateTaskScreen}
-                  />
-                  <Stack.Screen
-                    name="ManageTags"
-                    component={ManageTagsScreen}
-                  />
-                  <Stack.Screen name="CreateTag" component={CreateTagScreen} />
-                  <Stack.Screen name="Purchase" component={PurchaseScreen} />
-                </Stack.Group>
-              </Stack.Navigator>
-            </NavigationContainer>
-          </ThemeProvider>
-        </TagManageProvider>
-      </TaskListProvider>
-    </EventProvider>
+                <Stack.Screen name="CreateTask" component={CreateTaskScreen} />
+                <Stack.Screen name="ManageTags" component={ManageTagsScreen} />
+                <Stack.Screen name="CreateTag" component={CreateTagScreen} />
+                <Stack.Screen name="Purchase" component={PurchaseScreen} />
+              </Stack.Group>
+            </Stack.Navigator>
+          </NavigationContainer>
+        </ThemeProvider>
+      </TagManageProvider>
+    </TaskListProvider>
   );
 };
 
