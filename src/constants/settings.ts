e47eebf1f2016995f6dfd24418da98102cbc1bc4
azamataltymyshev <aza.alt.com@gmail.com>
2023-04-07 13:21:37 +0300

    Redesign of Languages Screen

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,6 +1,6 @@
 import { Chat, Heart, Image, Send, ShieldDone, Star } from '@/components/icons';
 
-export const ITEMS_LIST = [
+export const SETTINGS_LIST = [
   { navigationName: 'Language', prependIcon: Chat, title: 'TAGS' },
   { navigationName: 'TagsSettings', prependIcon: Image, title: 'THEME' },
   { navigationName: 'AppIcon', prependIcon: Heart, title: 'APP_ICON' },
