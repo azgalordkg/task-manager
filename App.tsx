9b961f6085d88b0604cbe8bced4e030e073f76fe
azamataltymyshev <aza.alt.com@gmail.com>
2023-04-07 19:38:56 +0300

    Redesign of AboutUs, Documents Screens

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ const App: FC = () => {
             <ThemeProvider>
               <NavigationContainer>
                 <Stack.Navigator
-                  initialRouteName="Settings"
+                  initialRouteName="Home"
                   screenOptions={{
                     headerShown: false,
                   }}>
