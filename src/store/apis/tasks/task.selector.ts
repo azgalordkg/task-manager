6e1c9d2a2e50a8db620062955fba4b9abe1f39b8
azamataltymyshev <aza.alt.com@gmail.com>
2023-05-08 00:29:07 +0300

    TNJ-28: move tasks selector

--- src/store/apis/tasks/task.selector.ts
+++ src/store/apis/tasks/task.selector.ts
@@ -2,7 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 
 import { RootState } from '@/store';
 
-export const selectedTasks = createSelector(
+export const selectTasks = createSelector(
   (state: RootState) => state.tasks.queries['getTasks()'],
   apiResult => apiResult?.data,
 );
