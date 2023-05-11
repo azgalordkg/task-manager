eb34a5c7fff3a1cf1afebb669baa26db7ab5dc26
Ruslan <boogaaga@gmail.com>
2023-05-11 16:34:29 +0600

    TNJ-65: delete getErrorMessage function

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,4 @@
 import { TOKEN } from '@/constants';
-import { ServerError } from '@/types';
 import { Storage } from '@/utils/storage';
 
 export const prepareHeaders = async (headers: Headers) => {
@@ -10,10 +9,4 @@ export const prepareHeaders = async (headers: Headers) => {
   }
 
   return headers;
-};
-
-export const getErrorMessage = (...errors: ServerError[]) => {
-  const message = errors.find(error => error?.data?.message)?.data?.message;
-
-  return message || 'SOMETHING_WENT_WRONG';
 };
